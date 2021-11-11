6ef6b063926074a73b019c2a401a364402bc68c9
Robin <sysatom@gmail.com>
2021-11-11 14:58:39 +0800

    feat: settings ui

--- src/screens/Settings/SettingsScreen.js
+++ src/screens/Settings/SettingsScreen.js
@@ -1,21 +1,90 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, TouchableOpacity} from 'react-native';
 import {useRootStore} from '../../stores/root';
-import {Button, Center} from 'native-base';
+import {
+  Avatar,
+  Box,
+  Center,
+  CloseIcon,
+  Divider,
+  Flex,
+  HamburgerIcon,
+  HStack,
+  InfoOutlineIcon,
+  QuestionIcon,
+  ThreeDotsIcon,
+} from 'native-base';
+import styles from './SettingsScreen.style';
 
 const SettingsScreen = ({navigation, route}) => {
   const {authStore} = useRootStore();
 
   return (
-    <Center safeArea>
-      <Text style={{textAlign: 'center', margin: 15}}>This is Settings</Text>
-      <Button
+    <Box>
+      <HStack alignItems="center">
+        <Avatar
+          source={{
+            uri: 'https://demo.com/demo.jpg',
+          }}
+          margin={3}>
+          a
+          <Avatar.Badge bg="green.500" />
+        </Avatar>
+        <Box>
+          <Text style={{fontSize: 13}}>Admin</Text>
+          <Text style={{fontSize: 11}}>admin@tsundata.com</Text>
+        </Box>
+      </HStack>
+      <Divider />
+
+      <TouchableOpacity>
+        <Flex style={styles.settingItem}>
+          <Box ml={5}>Notification Preferences</Box>
+          <InfoOutlineIcon mr={5} size={5} />
+        </Flex>
+      </TouchableOpacity>
+      <Divider />
+
+      <TouchableOpacity>
+        <Flex style={styles.settingItem}>
+          <Box ml={5}>Change language</Box>
+          <HamburgerIcon mr={5} size={5} />
+        </Flex>
+      </TouchableOpacity>
+      <Divider />
+
+      <TouchableOpacity>
+        <Flex style={styles.settingItem}>
+          <Box ml={5}>Help Docs</Box>
+          <QuestionIcon mr={5} size={5} />
+        </Flex>
+      </TouchableOpacity>
+      <Divider />
+
+      <TouchableOpacity>
+        <Flex style={styles.settingItem}>
+          <Box ml={5}>Chat with Us</Box>
+          <ThreeDotsIcon mr={5} size={5} />
+        </Flex>
+      </TouchableOpacity>
+      <Divider />
+
+      <TouchableOpacity
         onPress={() => {
           authStore.logout();
         }}>
-        Logout
-      </Button>
-    </Center>
+        <Flex style={styles.settingItem}>
+          <Box ml={5}>Logout</Box>
+          <CloseIcon mr={5} size={5} />
+        </Flex>
+      </TouchableOpacity>
+      <Divider />
+
+      <Center mt={10}>
+        <Text style={styles.logo}>Flow</Text>
+      </Center>
+      <Center>v1.0</Center>
+    </Box>
   );
 };
 
---
+++ src/screens/Settings/SettingsScreen.style.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  settingItem: {
+    height: 60,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  logo: {
+    fontSize: 30,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+});
+
+export default styles;
